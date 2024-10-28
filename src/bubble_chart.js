a48b4dda6e5b527d0898e52f5e4e263a625860b2
Yuichi Yazaki <visualizingjp@gmail.com>
2024-10-28 16:05:23 +0900

    コードの相殺

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -1,3 +1,6 @@
+
+
+
 function bubbleChart() {
 
       var width = 940;
@@ -32,6 +35,12 @@ function bubbleChart() {
     .force('x', d3.forceX().strength(forceStrength).x(center.x))
     .force('y', d3.forceY().strength(forceStrength).y(center.y))
 
+      function charge(d) {
+        return -Math.pow(d.radius, 2.0) * forceStrength;
+      }
+
+
+  
       var simulation = d3.forceSimulation()
         .velocityDecay(0.2) // ノード（バブル）の移動速度の減衰率を設定
         .force('charge', d3.forceManyBody().strength(charge))
@@ -44,17 +53,16 @@ function bubbleChart() {
         .range(['#d84b2a', '#beccae', '#7aa25c']);
 
 
-  function createNodes(rawData) {
-    var maxAmount = d3.max(rawData, function (d) { return +d.total_amount; });
 
+      function createNodes(rawData) {
 
+          var maxAmount = d3.max(rawData, function (d) { return +d.total_amount; });
 
-      function charge(d) {
-        return -Math.pow(d.radius, 2.0) * forceStrength;
           var radiusScale = d3.scalePow()
             .exponent(0.5)
             .range([2, 85])
             .domain([0, maxAmount]);
+
           var myNodes = rawData.map(function (d) {
             return {
               id: d.id,
@@ -66,130 +74,161 @@ function bubbleChart() {
               year: d.start_year,
               x: Math.random() * 900,
               y: Math.random() * 800
+            };
+          });
+
+          // sort them to prevent occlusion of smaller nodes.
+          myNodes.sort(function (a, b) { return b.value - a.value; });
+
+          return myNodes;
       }
+
+
+
+      var chart = function chart(selector, rawData) {
+
+            nodes = createNodes(rawData);
+
+            svg = d3.select(selector)
+              .append('svg')
+              .attr('width', width)
+              .attr('height', height);
+
+            bubbles = svg.selectAll('.bubble')
+              .data(nodes, function (d) { return d.id; });
+
+            var bubblesE = bubbles.enter().append('circle')
+              .classed('bubble', true)
+              .attr('r', 0)
+              .attr('fill', function (d) { return fillColor(d.group); })
+              .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+              .attr('stroke-width', 2)
+              .on('mouseover', showDetail)
+              .on('mouseout', hideDetail);
+
+            bubbles = bubbles.merge(bubblesE);
+
+            bubbles.transition()
+              .duration(2000)
+              .attr('r', function (d) { return d.radius; });
+
+            simulation.nodes(nodes);
+
+            groupBubbles();
       };
-    });
 
-    myNodes.sort(function (a, b) { return b.value - a.value; });
 
-    return myNodes;
-  }
 
-  var chart = function chart(selector, rawData) {
-    nodes = createNodes(rawData);
+      function ticked() {
+        bubbles
+          .attr('cx', function (d) { return d.x; })
+          .attr('cy', function (d) { return d.y; });
+      }
 
-    svg = d3.select(selector)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
 
-    bubbles = svg.selectAll('.bubble')
-      .data(nodes, function (d) { return d.id; });
 
-    var bubblesE = bubbles.enter().append('circle')
-      .classed('bubble', true)
-      .attr('r', 0)
-      .attr('fill', function (d) { return fillColor(d.group); })
-      .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
-      .attr('stroke-width', 2)
-      .on('mouseover', showDetail)
-      .on('mouseout', hideDetail);
+      function nodeYearPos(d) {
+        return centersSplit[d.year].x;
+      }
 
-    bubbles = bubbles.merge(bubblesE);
 
-    bubbles.transition()
-      .duration(2000)
-      .attr('r', function (d) { return d.radius; });
 
-    simulation.nodes(nodes);
+      function groupBubbles() {
+        hideYearTitles();
 
-    groupBubbles();
-  };
+        // @v4 Reset the 'x' force to draw the bubbles to the center.
+        simulation.force('x', d3.forceX().strength(forceStrength).x(centersGroup.x));
 
-  function ticked() {
-    bubbles
-      .attr('cx', function (d) { return d.x; })
-      .attr('cy', function (d) { return d.y; });
-  }
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+      }
 
   function nodeYearPos(d) {
     return yearCenters[d.year].x;
   }
 
 
-  function groupBubbles() {
-    hideYearTitles();
+      function splitBubbles() {
+        showYearTitles();
 
     simulation.force('x', d3.forceX().strength(forceStrength).x(center.x));
+        // @v4 Reset the 'x' force to draw the bubbles to their year centers
 
-    simulation.alpha(1).restart();
-  }
-
-
-  function splitBubbles() {
-    showYearTitles();
-
-    simulation.force('x', d3.forceX().strength(forceStrength).x(nodeYearPos));
-
-    simulation.alpha(1).restart();
-  }
-
-  function hideYearTitles() {
-    svg.selectAll('.year').remove();
-  }
-
-  function showYearTitles() {
-    var yearsData = d3.keys(yearsTitleX);
-    var years = svg.selectAll('.year')
-      .data(yearsData);
-
-    years.enter().append('text')
-      .attr('class', 'year')
-      .attr('x', function (d) { return yearsTitleX[d]; })
-      .attr('y', 40)
-      .attr('text-anchor', 'middle')
-      .text(function (d) { return d; });
-  }
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+      }
 
 
-  function showDetail(d) {
-    d3.select(this).attr('stroke', 'black');
-
-    var content = '<span class="name">Title: </span><span class="value">' +
-                  d.name +
-                  '</span><br/>' +
-                  '<span class="name">Amount: </span><span class="value">$' +
-                  addCommas(d.value) +
-                  '</span><br/>' +
-                  '<span class="name">Year: </span><span class="value">' +
-                  d.year +
-                  '</span>';
-
-    tooltip.showTooltip(content, d3.event);
-  }
-
-  function hideDetail(d) {
-    // reset outline
-    d3.select(this)
-      .attr('stroke', d3.rgb(fillColor(d.group)).darker());
-
-    tooltip.hideTooltip();
-  }
-
-  chart.toggleDisplay = function (displayName) {
-    if (displayName === 'year') {
-      splitBubbles();
-    } else {
-      groupBubbles();
-    }
-  };
+      function hideYearTitles() {
+        svg.selectAll('.year').remove();
+      }
 
 
-  return chart;
+      function showYearTitles() {
+        // Another way to do this would be to create
+        // the year texts once and then just hide them.
+        var yearsData = d3.keys(yearsTitleX);
+        var years = svg.selectAll('.year')
+          .data(yearsData);
+
+        years.enter().append('text')
+          .attr('class', 'year')
+          .attr('x', function (d) { return yearsTitleX[d]; })
+          .attr('y', 40)
+          .attr('text-anchor', 'middle')
+          .text(function (d) { return d; });
+      }
+
+
+
+      function showDetail(d) {
+
+        d3.select(this).attr('stroke', 'black');
+
+        var content = '<span class="name">Title: </span><span class="value">' +
+                      d.name +
+                      '</span><br/>' +
+                      '<span class="name">Amount: </span><span class="value">$' +
+                      addCommas(d.value) +
+                      '</span><br/>' +
+                      '<span class="name">Year: </span><span class="value">' +
+                      d.year +
+                      '</span>';
+
+        tooltip.showTooltip(content, d3.event);
+      }
+
+
+
+
+      function hideDetail(d) {
+        // reset outline
+        d3.select(this)
+          .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+
+        tooltip.hideTooltip();
+      }
+
+
+
+
+      chart.toggleDisplay = function (displayName) {
+        if (displayName === 'year') {
+          splitBubbles();
+        } else {
+          groupBubbles();
+        }
+      };
+
+      return chart;
 }
 
 
+
+
 var myBubbleChart = bubbleChart();
+
+
 
 function display(error, data) {
   if (error) {
@@ -199,20 +238,28 @@ function display(error, data) {
   myBubbleChart('#vis', data);
 }
 
+
+
 function setupButtons() {
   d3.select('#toolbar')
     .selectAll('.button')
     .on('click', function () {
+      
       // Remove active class from all buttons
       d3.selectAll('.button').classed('active', false);
+      
+      // Find the button just clicked & set it as the active button
       var button = d3.select(this);
       button.classed('active', true);
 
-      // Get the id of the button
+      // buttonIdによって、表示内容のフラグとする
       var buttonId = button.attr('id');
       myBubbleChart.toggleDisplay(buttonId);
+      
     });
 }
+
+
 
 function addCommas(nStr) {
   nStr += '';
@@ -226,6 +273,8 @@ function addCommas(nStr) {
 
   return x1 + x2;
 }
+
+
 
 // Load the data.
 d3.csv('data/gates_money.csv', display);
